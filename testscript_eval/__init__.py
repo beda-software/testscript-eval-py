140af2fd472476d5ec2076df87b3be405d14235f
Ilya Beda <ir4y.ix@gmail.com>
2023-10-20 17:55:28 +1100

    Bump 0.0.4

--- testscript_eval/__init__.py
+++ testscript_eval/__init__.py
@@ -1,7 +1,7 @@
 from .eval import eval
 
 __title__ = "testscript_eval"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __author__ = "beda.software"
 __license__ = "MIT License"
 __copyright__ = "Copyright 2023 beda.software"
