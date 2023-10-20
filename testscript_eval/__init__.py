143c1b0a88277970750b29d58768f7534b16c719
Ilya Beda <ir4y.ix@gmail.com>
2023-10-20 16:21:24 +1100

    Add publish workflow

--- testscript_eval/__init__.py
+++ testscript_eval/__init__.py
@@ -1,1 +1,10 @@
 from .eval import eval
+
+__title__ = "testscript_eval"
+__version__ = "0.0.1"
+__author__ = "beda.software"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2023 beda.software"
+
+# Version synonym
+VERSION = __version__
